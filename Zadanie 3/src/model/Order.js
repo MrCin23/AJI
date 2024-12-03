f9313c97179e3266ca3041895f416cb3bb23eb3d
MrCin23 <marcin.targonski23@gmail.com>
2024-12-03 15:14:27 +0100

    REST API structure done, project works. TODO validation and another buisness model methods

--- Zadanie 3/src/model/Order.js
+++ Zadanie 3/src/model/Order.js
@@ -9,35 +9,36 @@ class Order extends Model {
 
     static get relationMappings() {
         return {
-            status: {
+            order_status: {
                 relation: Model.BelongsToOneRelation,
                 modelClass: OrderStatus,
                 join: {
                     from: 'orders.status_id',
-                    to: 'order_statuses.id',
+                    to: 'order_status.id',
                 },
             },
-            orderedItems: {
-                relation: Model.HasManyRelation,
-                modelClass: Product,
-                join: {
-                    from: 'orders.id',
-                    to: 'product.order_id',
-                },
-            },
+            // ordered_items: {
+            //     relation: Model.HasManyRelation,
+            //     modelClass: Product,
+            //     join: {
+            //         from: 'orders.id',
+            //         to: 'product.id',
+            //     },
+            // },
         };
     }
+
 
     static get jsonSchema() {
         return {
             type: 'object',
-            required: ['username', 'email', 'phone_number', 'status_id', 'ordered_items'],
+            required: ['username', 'email', 'phone_number', 'status_id'],
 
             properties: {
                 id: { type: 'integer' },
-                username: { type: 'string' },
-                email: { type: 'string' },
-                phone_number: { type: 'string' },
+                username: { type: 'string', minLength: 1 },
+                email: { type: 'string', minLength: 1 },
+                phone_number: { type: 'string', minLength: 9, maxLength: 9 }, //Regex
                 status_id: { type: 'integer' },
                 approval_date: { type: 'string', format: 'date-time', nullable: true },
                 ordered_items: { type: 'array' },
--- Zadanie 3/src/repository/OrderRepository.js
+++ Zadanie 3/src/repository/OrderRepository.js
@@ -9,49 +9,119 @@ Model.knex(knex);
 
 class OrderRepository {
 
+
+
     async findAll() {
-        return Order.query()//.withGraphFetched('status, ordered_items');
+
+        const orders = await Order.query().withGraphFetched('order_status');
+
+        for (const order of orders) {
+            let items;
+
+
+            if (Array.isArray(order.ordered_items)) {
+                items = order.ordered_items;
+            } else if (typeof order.ordered_items === 'string') {
+                items = JSON.parse(order.ordered_items);
+            } else {
+                throw new Error(`Invalid format for ordered_items: ${order.ordered_items}`);
+            }
+
+            const productIds = items.map((item) => item.product_id);
+            const products = await Product.query().findByIds(productIds);
+
+            order.products = products.map((product) => ({
+                ...product,
+                quantity: items.find((item) => item.product_id === product.id).quantity,
+            }));
+        }
+
+        return orders;
     }
 
 
 
     async findByStatus(statusId) {
-        return Order.query()
-            .join('order_status', 'order.status_id', '=', 'order_status.id')
-            .where('order_status.id', statusId);
-            //.withGraphFetched('status, ordered_items');
+
+        const orders = await Order.query()
+            .withGraphFetched('order_status')
+            .where('status_id', statusId);
+
+
+        for (const order of orders) {
+            let items;
+
+            if (Array.isArray(order.ordered_items)) {
+                items = order.ordered_items;
+            } else if (typeof order.ordered_items === 'string') {
+                items = JSON.parse(order.ordered_items);
+            } else {
+                throw new Error(`Invalid format for ordered_items: ${order.ordered_items}`);
+            }
+
+            const productIds = items.map((item) => item.product_id);
+            const products = await Product.query().findByIds(productIds);
+
+
+            order.products = products.map((product) => ({
+                ...product,
+                quantity: items.find((item) => item.product_id === product.id).quantity,
+            }));
+        }
+
+        return orders;
     }
 
 
-
     async create(orderData) {
-        const { status_id, orderedItems, ...otherData } = orderData;
+        const { status_id, ordered_items, ...otherData } = orderData;
 
+        // Retrieve the status instance
         const statusInstance = await OrderStatus.query().findOne('id', status_id);
 
         if (!statusInstance) {
             throw new Error(`Invalid status: ${status_id}`);
         }
 
-        // const order = await Order.query().insertAndFetch({
-        //     ...otherData,
-        //     status_id: statusInstance.id,
-        //     ordered_items: ordered_items,
-        // });
-        return Product.query().insert(orderData);
+        const orderedItemsData = ordered_items.map(item => ({
+            product_id: item.product_id,
+            quantity: item.quantity,
+        }));
 
+
+        const products = await Promise.all(
+            orderedItemsData.map(async (order) => {
+                const product = await Product.query().findById(order.product_id).withGraphFetched('category');
+
+                product.unit_price = parseFloat(product.unit_price);
+                product.unit_weight = parseFloat(product.unit_weight);
+                return { ...product, quantity: order.quantity };
+            })
+        );
+
+
+        const order = await Order.query().insertAndFetch({
+            ...otherData,
+            status_id: statusInstance.id,
+            ordered_items: orderedItemsData,
+            // ordered_items: products.map((product) => ({
+            //     quantity: product.quantity,
+            //     product_id: product.id,
+            // }))
+        });
+
+        return order;
     }
 
-
-    async updateStatus(id, newStatus) {
-        const statusInstance = await OrderStatus.query().findOne('status', newStatus);
+    async updateStatus(id, newStatusID) {
+        const statusInstance = await OrderStatus.query().findOne('id', newStatusID.status_id);
 
         if (!statusInstance) {
-            throw new Error(`Invalid status: ${newStatus}`);
+            throw new Error(`Invalid status: ${newStatusID}`);
         }
 
         return Order.query().patchAndFetchById(id, {
-            statusId: statusInstance.id,
+            status_id: statusInstance.id,
         });
     }
 }
